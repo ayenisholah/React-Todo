c627805a2895264dac0f25acfde9498d9bb4ca8e
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-05 15:02:36 +0100

    Initial todo list created

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,18 @@
+import React, { Component, useState } from 'react';
+import uuid from 'uuid';
+const todoList = [
+    {
+      task: 'Organize Garage',
+      id: 1528817077286,
+      completed: false
+    },
+    {
+      task: 'Bake Cookies',
+      id: 1528817084358,
+      completed: false
+    }
+  ];
+
+  export class ContainerTodo extends Component {
+      
+  }
